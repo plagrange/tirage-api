1a02d3c1585916c03ce3871fdf1275b50a618af2
Patrick.MEKEZE.KWATCHO <5quccpogvx5p3pdf3q2cdpdaf4ajppfs5rdpyjm2jffkdxoam5ca>
2022-07-18 17:32:33 +0200

    Remove unnecessary exception declaration

--- src/main/java/com/lagrange/tirage/tirageapi/services/interfaces/ITirageCoreService.java
+++ src/main/java/com/lagrange/tirage/tirageapi/services/interfaces/ITirageCoreService.java
@@ -1,20 +1,22 @@
 package com.lagrange.tirage.tirageapi.services.interfaces;
 
 import com.lagrange.tirage.tirageapi.entity.Tirage;
+import com.lagrange.tirage.tirageapi.exceptions.SendMessageException;
 import com.lagrange.tirage.tirageapi.model.*;
 
+import javax.mail.MessagingException;
 import java.util.List;
 
 public interface ITirageCoreService {
 
-    List<UserResponse> initListParticipant(CreateTirageRequest createTirage) throws Exception;
-    boolean authenticateByDB(String email, String criteria, String company) throws Exception;
-    int doTirage(String email, String company) throws Exception;
-    boolean authenticateAdminByDB(String email, String criteria, String company) throws Exception;
-    List<UserTirageResponse> getResultList(String company) throws Exception;
+    List<UserResponse> initListParticipant(CreateTirageRequest createTirage);
+    boolean authenticateByDB(String email, String criteria, String company);
+    int doTirage(String email, String company);
+    boolean authenticateAdminByDB(String email, String criteria, String company);
+    List<UserTirageResponse> getResultList(String company);
     List<UserResource> getListParticipantFromDB(String company);
-    NotifyUserResponse notifyUser(NotifyUserResource userResource) throws Exception;
-    boolean verifyCompanyAlreadyExist(String company) throws Exception;
-    List<String> getListExistedCompany() throws Exception;
+    NotifyUserResponse notifyUser(NotifyUserResource userResource) throws MessagingException;
+    boolean verifyCompanyAlreadyExist(String company);
+    List<String> getListExistedCompany();
     Tirage verifyUserAlreadyDoTirage(String email, String company);
 }
