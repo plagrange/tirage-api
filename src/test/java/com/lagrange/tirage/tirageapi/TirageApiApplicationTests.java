7bccb5d64767a8886c18478d8046d6b8c778f5ab
Patrick.MEKEZE.KWATCHO <5quccpogvx5p3pdf3q2cdpdaf4ajppfs5rdpyjm2jffkdxoam5ca>
2022-07-19 00:36:11 +0200

    update springBootTest to use test profile

--- src/test/java/com/lagrange/tirage/tirageapi/TirageApiApplicationTests.java
+++ src/test/java/com/lagrange/tirage/tirageapi/TirageApiApplicationTests.java
@@ -2,15 +2,20 @@ package com.lagrange.tirage.tirageapi;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles({"test"})
 class TirageApiApplicationTests {
 
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	String dll;
 	@Test
 	void contextLoads() {
 		Assertions.assertTrue(true);
+		System.out.printf("dll = " + dll);
 	}
 
 }
